460a050d23fc2ba3f086b0017aba2ebdbb8ecba6
AverianovAndrew <79270072+AverianovAndrew@users.noreply.github.com>
2021-02-28 23:46:11 +0300

    Add files via upload

--- КонтрР/gnuplot.sh
+++ КонтрР/gnuplot.sh
@@ -1,5 +1,10 @@
+set output "Temp.png"
 plot "~/repos/lj/Temp.txt" using 1:2
-plot "~/repos/lj/temp_0.5/rdf.txt" using 1:2
-plot "~/repos/lj/temp_1/rdf.txt" using 1:2
-plot "~/repos/lj/temp_1.5/rdf.txt" using 1:2
-plot "~/repos/lj/temp_2/rdf.txt" using 1:2
+set output "RDF1.png"
+plot "~/repos/lj/temp_0.5/rdf.txt" using 1:3
+set output "RDF2.png"
+plot "~/repos/lj/temp_1/rdf.txt" using 1:3
+set output "RDF3.png"
+plot "~/repos/lj/temp_1.5/rdf.txt" using 1:3
+set output "RDF4.png"
+plot "~/repos/lj/temp_2/rdf.txt" using 1:3
