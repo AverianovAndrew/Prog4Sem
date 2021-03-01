fe73d1666d9535d464a1120d122b5da6bbb4233d
AverianovAndrew <79270072+AverianovAndrew@users.noreply.github.com>
2021-03-01 12:59:05 +0300

    Add files via upload

--- КонтрР/Contr.sh
+++ КонтрР/Contr.sh
@@ -3,19 +3,19 @@ mkdir lj
 cd lj
 touch Temp.txt
 echo "#Temp_initial Temp_average" >> Temp.txt
-mkdir temp_0.5
-mkdir temp_1
-mkdir temp_1.5
-mkdir temp_2
+for temp in 1 4;
+do
+let "tmp = temp * 0.5"
+let "tm = tmp - 0.5"
+cd ~/repos/lj
+mkdir temp_$tmp
 cd ..
-sed "s/all create 3.0/all create 0.5/g" in.melt
+sed "s/all create $tm/all create $tmp/g" in.melt
 srun -N 1 -p RT_study --ntasks-per-node=8 -J lammps_aver --comment="cw" ~/repos/lmp_mpi -in in.melt
 temp=$(awk -f awk.sh log.lammps)
 cd lj
-echo "0.5 $temp" >> Temp.txt
-cd ..
-cd lj
-cd temp_0.5
+echo "$tmp $temp" >> Temp.txt
+cd temp_$tmp
 touch rdf.txt
 echo "#N r RDF" >> rdf.txt
 tail -n 50 tmp.rdf >> rdf.txt
@@ -24,54 +24,7 @@ cd ..
 rm dump.melt
 rm tmp.rdf
 rm log.lammps
-sed "s/all create 0.5/all create 1/g" in.melt
-srun -N 1 -p RT_study --ntasks-per-node=8 -J lammps_aver --comment="cw" ~/repos/lmp_mpi -in in.melt
-temp=$(awk -f awk.sh log.lammps)
-cd lj
-echo "1 $temp" >> Temp.txt
-cd ..
-cd lj
-cd temp_1
-touch rdf.txt
-echo "#N r RDF" >> rdf.txt
-tail -n 50 tmp.rdf >> rdf.txt
-cd ..
-cd ..
-rm dump.melt
-rm tmp.rdf
-rm log.lammps
-sed "s/all create 1/all create 1.5/g" in.melt
-srun -N 1 -p RT_study --ntasks-per-node=8 -J lammps_aver --comment="cw" ~/repos/lmp_mpi -in in.melt
-temp=$(awk -f awk.sh log.lammps)
-cd lj
-echo "1.5 $temp" >> Temp.txt
-cd ..
-cd lj
-cd temp_1.5
-touch rdf.txt
-echo "#N r RDF" >> rdf.txt
-tail -n 50 tmp.rdf >> rdf.txt
-cd ..
-cd ..
-rm dump.melt
-rm tmp.rdf
-rm log.lammps
-sed "s/all create 1.5/all create 2/g" in.melt
-srun -N 1 -p RT_study --ntasks-per-node=8 -J lammps_aver --comment="cw" ~/repos/lmp_mpi -in in.melt
-temp=$(awk -f awk.sh log.lammps)
-cd lj
-cd lj
-echo "2 $temp" >> Temp.txt
-cd ..
-cd lj
-cd temp_2
-touch rdf.txt
-echo "#N r RDF" >> rdf.txt
-tail -n 50 tmp.rdf >> rdf.txt
-cd ..
-cd ..
-rm dump.melt
-rm tmp.rdf
-rm log.lammps
+done
 gnuplot gnuplot.sh
+
 
